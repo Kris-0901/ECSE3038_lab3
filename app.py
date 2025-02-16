4a0f2b11fa424c79cf4552170dd87062802ccf14
Kris-0901 <tucker.kristof@gmail.com>
2025-02-16 11:29:15 -0500

    successful POST request

--- app.py
+++ app.py
@@ -12,13 +12,27 @@ class Tank(BaseModel):
     id:UUID = Field(default_factory=uuid4)
     location:str
     lat:float
-    lon:float
+    long:float
 
 class Tank_Update(BaseModel):
     location:str | None = None
     lat:float | None = None
-    lon:float | None = None
+    long:float | None = None
 
     @app.get("/tank")
     async def get_all_tanks():
         return data
+    
+    @app.post("/tank")
+    async def add_tank(new_tank:Tank):
+
+        if not new_tank.location:
+            raise HTTPException(status_code=422,detail="Invalid request: 'location' filed missing ")
+        
+        if new_tank.lat is None or new_tank.lat is None: 
+            raise HTTPException(status_code=422,detail="Invalid request: 'lat' or 'long' filed missing")
+        
+        tank_json= jsonable_encoder(new_tank)
+        data.append(new_tank)
+        return JSONResponse(tank_json,status_code=201)
+        
