408cb1a9654dc7ff96ac53c739d85a81dcad673d
Kris-0901 <tucker.kristof@gmail.com>
2025-02-16 12:20:33 -0500

    successful PATCH request

--- app.py
+++ app.py
@@ -19,28 +19,44 @@ class Tank_Update(BaseModel):
     lat:float | None = None
     long:float | None = None
 
-    @app.get("/tank")
-    async def get_all_tanks():
-        return data
+@app.get("/tank")
+async def get_all_tanks():
+    return data
+
+@app.post("/tank")
+async def add_tank(new_tank:Tank):
+
+    if not new_tank.location:
+        raise HTTPException(status_code=422,detail="Invalid request: 'location' filed missing ")
     
-    @app.post("/tank")
-    async def add_tank(new_tank:Tank):
+    if new_tank.lat is None or new_tank.lat is None: 
+        raise HTTPException(status_code=422,detail="Invalid request: 'lat' or 'long' filed missing")
+    
+    tank_json= jsonable_encoder(new_tank)
+    data.append(new_tank)
+    return JSONResponse(tank_json,status_code=201)
 
-        if not new_tank.location:
-            raise HTTPException(status_code=422,detail="Invalid request: 'location' filed missing ")
-        
-        if new_tank.lat is None or new_tank.lat is None: 
-            raise HTTPException(status_code=422,detail="Invalid request: 'lat' or 'long' filed missing")
-        
-        tank_json= jsonable_encoder(new_tank)
-        data.append(new_tank)
-        return JSONResponse(tank_json,status_code=201)
+@app.get("/tank{id}")
+async def find_tank(id:UUID):
+    for tank in data:
+        if tank.id == id:
+            tank_json=jsonable_encoder(tank)
+            return JSONResponse(tank_json,status_code=200)
+        raise HTTPException(status_code=404, detail="Tank not found")
+    
+@app.patch("/tank/{id}")
+async def update_tank(id:UUID,updated_tank:Tank_Update):
+    for i, tank in enumerate(data):
+        if tank.id == id:
+            tank_upadte_dict=updated_tank.model_dump(exclude_unset=True)
 
-    @app.get("/tank{id}")
-    async def find_tank(id:UUID):
-        for tank in data:
-            if tank.id == id:
-                tank_json=jsonable_encoder(tank)
-                return JSONResponse(tank_json,status_code=200)
-            raise HTTPException(status_code=404, detail="Tank not found")
+            try: 
+                new_updated_tank=tank.copy(update=tank_upadte_dict)
+                data[i]=Tank.model_validate(new_updated_tank)
+                json_new_updated_tank=jsonable_encoder(new_updated_tank)
+                return JSONResponse(json_new_updated_tank,status_code=200)
+            except ValidationError:
+                raise HTTPException(status_code=400, detail="Invalid Request: Tank should consist of a 'location', 'lat' or 'long'")
+        raise HTTPException(status_code=400, detail="Tank not found")
+
         
